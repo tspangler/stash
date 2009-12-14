9848c22a9ec27f74ab21f86f9a23e1204529ef63
Tim <tim@timspangler.com>
2009-12-14 16:16:38 -0600

    Functionality works, though one does have to mv keyfile.ez keyfile before decrypting at the moment

--- stash.rb
+++ stash.rb
@@ -2,12 +2,15 @@ require 'rubygems'
 require 'ezcrypto'
 require 'digest/sha1'
 
+HASH = 'pes'
+KEYFILE_NAME = 'keyfile'
+
 def get_directory_list
   all_dirs = Dir.new('.').entries
   file_list = Array.new
   
   all_dirs.each do |current_dir|
-    if !File.directory?(current_dir) && current_dir != __FILE__
+    if !File.directory?(current_dir) && current_dir != __FILE__ && current_dir != KEYFILE_NAME
       file_list.push(current_dir)
     end
   end
@@ -20,31 +23,67 @@ def assemble_directory_hash
 end
 
 def write_keyfile
-  keyfile = File.new('keyfile', 'w')
+  keyfile = File.new(KEYFILE_NAME, 'w')
 
   get_directory_list.each do |filename|
     keyfile.puts "#{Digest::SHA1.hexdigest(filename)}|#{filename}"
+    
+    # Rename the file to the hash
+    if File.exists?(filename)
+      File.rename(filename, Digest::SHA1.hexdigest(filename))
+    end
   end
 
   keyfile.close
 end
 
 def encrypt_keyfile(the_key)
-  key = EzCrypto::Key.with_password the_key, "pes"
-  key.encrypt_file('keyfile', 'keyfilee', { :suffix => '.kye' })
+  key = EzCrypto::Key.with_password the_key, HASH
+  key.encrypt_file(KEYFILE_NAME)
+end
+
+def decrypt_keyfile(the_key)
+  key = EzCrypto::Key.with_password the_key, HASH
+  key.decrypt_file(KEYFILE_NAME, KEYFILE_NAME + '_dec')
+end
+
+def hash_filename
+  
+end
+
+def restore_filenames
+  # Make sure the keyfile exists
+  if !File.exists?(KEYFILE_NAME + '_dec')
+    return false
+  end
+  
+  # Go through the file line by line and do the rename
+  File.open(KEYFILE_NAME + '_dec') do |file|
+    while current_line = file.gets
+      # Magic voodoo here
+      File.rename(current_line.split('|')[0], current_line.split('|')[1].chomp)
+    end
+  end
+  
+  return true
 end
 
 print "Key? "
 the_key = gets.chomp
 
-if !write_keyfile
-  puts 'Keyfile could not be written.'
+# Determine if encrypting or decrypting
+if File.exists?(KEYFILE_NAME)
+  puts 'Decrypting keyfile...'
+  decrypt_keyfile(the_key)
+
+  if restore_filenames
+    puts 'Successfully restored filenames.'
+  else
+    puts 'Could not restore filenames.'
+  end
 else
-  puts 'Keyfile written.'
+  puts 'Writing keyfile and renaming files...'
+  write_keyfile
+  encrypt_keyfile(the_key)
 end
 
-if !encrypt_keyfile(the_key)
-  puts 'Keyfile could not be encrypted.'
-else
-  puts 'Keyfile encrypted.'
-end
