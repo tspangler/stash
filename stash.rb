1966c81b47e35b2d11a6300626f85afab2885f9b
Timothy Spangler <symsonic@mark.dreamhost.com>
2009-12-14 10:11:00 -0800

    Added all encryption functionality.

--- stash.rb
+++ stash.rb
@@ -1,12 +1,13 @@
 require 'rubygems'
 require 'ezcrypto'
+require 'digest/sha1'
 
 def get_directory_list
   all_dirs = Dir.new('.').entries
   file_list = Array.new
   
   all_dirs.each do |current_dir|
-    if !File.directory?(current_dir)
+    if !File.directory?(current_dir) && current_dir != __FILE__
       file_list.push(current_dir)
     end
   end
@@ -14,15 +15,36 @@ def get_directory_list
   file_list
 end
 
+def assemble_directory_hash
+
+end
+
+def write_keyfile
+  keyfile = File.new('keyfile', 'w')
+
+  get_directory_list.each do |filename|
+    keyfile.puts "#{Digest::SHA1.hexdigest(filename)}|#{filename}"
+  end
+
+  keyfile.close
+end
+
+def encrypt_keyfile(the_key)
+  key = EzCrypto::Key.with_password the_key, "pes"
+  key.encrypt_file('keyfile', 'keyfilee', { :suffix => '.kye' })
+end
+
 print "Key: "
 the_key = gets.chomp
 
-key = EzCrypto::Key.with_password the_key, "pes"
-encrypted_content = key.encrypt "At the end of my 80, I'll return to the dirt."
+if !write_keyfile
+  puts 'Keyfile could not be written.'
+else
+  puts 'Keyfile written.'
+end
 
-puts encrypted_content
-
-puts 'Decrypting...'
-decrypted_content = key.decrypt encrypted_content
-
-puts get_directory_list.inspect
+if !encrypt_keyfile(the_key)
+  puts 'Keyfile could not be encrypted.'
+else
+  puts 'Keyfile encrypted.'
+end
